3bbb20135492fffe8295783d0e7d1b4ad5202045
guilhersantos949 <guilhermemans@outlook.com>
2025-03-13 01:32:21 -0300

    atualização

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const lista = document.querySelectorAll('.lista');
+
+function ativaLink(){
+    for(let i of lista){
+        i.classList.remove('ativo')
+    }
+    this.classList.add('ativo')
+}
+
+for(let i of lista){
+    i.addEventListener('click', ativalink)
+}
